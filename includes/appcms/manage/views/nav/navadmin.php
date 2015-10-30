56e445cd4a94c85bdd54a5506180975797f01adf
iksgroupco <eyubalzary@gmail.com>
2015-10-31 00:52:21 +0700

    Add Navigation Operator and Fix Nav

--- includes/appcms/manage/views/nav/navadmin.php
+++ includes/appcms/manage/views/nav/navadmin.php
@@ -1,3 +1,26 @@
+<li class="treeview <?=menuActive("media");?>">
+    <a href="#"><i class="fa fa-picture-o"></i> <span>Galeri Album</span> <i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+        <li><a href="<?=base_url(roleURIUser().'media/album');?>"><i class="fa fa-circle-o"></i> Semua Album</a></li>
+        <li><a href="<?=base_url(roleURIUser().'media/album/add');?>"><i class="fa fa-circle-o"></i> Tambah Album</a></li>
+        <li><a href="<?=base_url(roleURIUser().'media/gallery');?>"><i class="fa fa-circle-o"></i> Semua Galeri</a></li>
+        <li><a href="<?=base_url(roleURIUser().'media/gallery/add');?>"><i class="fa fa-circle-o"></i> Tambah Galeri</a></li>
+    </ul>
+</li>
+<li class="treeview <?=menuActive("content","events");?>">
+    <a href="#"><i class="fa fa-calendar"></i> <span>Events</span> <i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+        <li><a href="<?=base_url(roleURIUser().'content/events');?>"><i class="fa fa-circle-o"></i> Semua Event</a></li>
+        <li><a href="<?=base_url(roleURIUser().'content/events/add');?>"><i class="fa fa-circle-o"></i> Tambah Event</a></li>
+    </ul>
+</li>
+<li class="treeview <?=menuActive("content","pages");?>">
+    <a href="#"><i class="fa fa-files-o"></i> <span>Halaman</span> <i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+        <li><a href="<?=base_url(roleURIUser().'content/pages');?>"><i class="fa fa-circle-o"></i> <?=langGet("menu","menu_pages_all");?></a></li>
+        <li><a href="<?=base_url(roleURIUser().'content/pages/add');?>"><i class="fa fa-circle-o"></i> <?=langGet("menu","menu_pages_add");?></a></li>
+    </ul>
+</li>
 <li class="treeview <?=menuActive("style");?>">
     <a href="#"><i class="fa fa-paint-brush"></i> <span><?=langGet("menu","menu_appearance");?></span> <i class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
--- includes/appcms/manage/views/nav/navcontainer.php
+++ includes/appcms/manage/views/nav/navcontainer.php
@@ -8,34 +8,14 @@
         <li><a href="<?=base_url(roleURIUser().'content/tags');?>"><i class="fa fa-circle-o"></i> <?=langGet("menu","menu_news_tags");?></a></li>
     </ul>
 </li>
-<li class="treeview <?=menuActive("media");?>">
-    <a href="#"><i class="fa fa-picture-o"></i> <span>Galeri Album</span> <i class="fa fa-angle-left pull-right"></i></a>
-    <ul class="treeview-menu">
-        <li><a href="<?=base_url(roleURIUser().'media/album');?>"><i class="fa fa-circle-o"></i> Semua Album</a></li>
-        <li><a href="<?=base_url(roleURIUser().'media/album/add');?>"><i class="fa fa-circle-o"></i> Tambah Album</a></li>
-        <li><a href="<?=base_url(roleURIUser().'media/gallery');?>"><i class="fa fa-circle-o"></i> Semua Galeri</a></li>
-        <li><a href="<?=base_url(roleURIUser().'media/gallery/add');?>"><i class="fa fa-circle-o"></i> Tambah Galeri</a></li>
-    </ul>
-</li>
-<li class="treeview <?=menuActive("content","events");?>">
-    <a href="#"><i class="fa fa-calendar"></i> <span>Events</span> <i class="fa fa-angle-left pull-right"></i></a>
-    <ul class="treeview-menu">
-        <li><a href="<?=base_url(roleURIUser().'content/events');?>"><i class="fa fa-circle-o"></i> Semua Event</a></li>
-        <li><a href="<?=base_url(roleURIUser().'content/events/add');?>"><i class="fa fa-circle-o"></i> Tambah Event</a></li>
-    </ul>
-</li>
-<li class="treeview <?=menuActive("content","pages");?>">
-    <a href="#"><i class="fa fa-files-o"></i> <span>Halaman</span> <i class="fa fa-angle-left pull-right"></i></a>
-    <ul class="treeview-menu">
-        <li><a href="<?=base_url(roleURIUser().'content/pages');?>"><i class="fa fa-circle-o"></i> <?=langGet("menu","menu_pages_all");?></a></li>
-        <li><a href="<?=base_url(roleURIUser().'content/pages/add');?>"><i class="fa fa-circle-o"></i> <?=langGet("menu","menu_pages_add");?></a></li>
-    </ul>
-</li>
+
 
 <?php
 if(roleUser()=="mimin"){
 	include dirname(__FILE__).'/'.'navadmin.php';
 }elseif(roleUser()=="editor"){
 	include dirname(__FILE__).'/'.'naveditor.php';
+}elseif(roleUser()=="op"){
+	include dirname(__FILE__).'/'.'navop.php';
 }
 ?>
