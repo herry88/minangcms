11ab801eda2e3cde3815eac8635f534711e9933c
iksgroupco <eyubalzary@gmail.com>
2015-11-02 08:51:06 +0700

    Global Menu View

--- includes/appcms/manage/views/style/menu/menuview.php
+++ includes/appcms/manage/views/style/menu/menuview.php
@@ -1,24 +1,32 @@
+<?php
+$themeActive=optionGet('theme_front');
+?>
 <div class="row">
 <div class="col-md-4">
 	<label>Pilih Menu</label>
-	<select name="menu" class="form-control" onchange="window.location='<?=base_url(roleURIUser().'style/menus?open=');?>'+this.value">
+	<select name="menu" class="form-control" id="menu">
 		<?php
 		$lastmenu=$this->input->get('open');
-		$lastmenu=isset($lastmenu) ? $lastmenu : "top";
-		if($lastmenu=="top"){
+		if(!empty($lastmenu)){
 			?>
-			<option value="top">Menu Atas</option>
-			<option value="bottom">Menu Bawah</option>
+			<option value="<?=$lastmenu;?>"><?=ucfirst($lastmenu);?></option>
 			<?php
-		}elseif($lastmenu=="bottom"){
+		}
+		?>
+		<option value="">-Pilih Menu-</option>
+		<?php
+		$lastmenu=$this->input->get('open');
+		$s=array(
+		'name LIKE '=>$themeActive."_theme_menu%",
+		);
+		$d=$this->m_database->fetchData('terms',$s);
+		foreach($d as $r){
+			$prefix=$themeActive."_theme_menu_";
+			$sanit=str_replace($prefix,"",$r->name);
+			$val=strtolower($sanit);
+			$label=ucfirst($sanit);
 			?>
-			<option value="bottom">Menu Bawah</option>
-			<option value="top">Menu Atas</option>		
-			<?php
-		}else{
-			?>
-			<option value="bottom">Menu Bawah</option>
-			<option value="top">Menu Atas</option>
+			<option value="<?=$val;?>"><?=$label;?></option>
 			<?php
 		}
 		?>		
@@ -26,7 +34,7 @@
 </div>
 <input type="hidden" name="menuposition" id="menuposition" value="<?=$lastmenu;?>"/>
 <div class="col-md-8">
-	Menu Atas dan Menu Bawah disesuaikan dengan template. Jadi, menu atas dan menu bawah belum tentu berada pada posisi atas atau bawah (dapat disesuaikan). <br> <strong class="text-danger">(*) Gunakan custom link jika menu mempunyai item baru</strong>
+	Posisi menu sama dengan posisi yang dikonfigurasikan pada tema aktif <b><?=ucfirst($themeActive);?></b>
 </div>
 </div>
 <p>&nbsp;</p>
@@ -34,26 +42,39 @@
 <div class="col-md-4">
 	<?php $this->load->view('style/menu/sidebartool');?>
 </div>
-<div class="col-md-8" id="menuloader" style="display: none;overflow: auto;">
+<div class="col-md-8" id="menuloader" style="display: none;">
 	
 </div>
 </div>
 <script>
 function getMenu(){
-	$.ajax({
-		type:'get',
-		dataType:'html',
-		url:'<?=base_url(roleURIUser()."style/menus/getmenu");?>',
-		data:'menu=<?=$lastmenu;?>',
-		beforeSend:function(){			
-		},
-		success:function(x){			
-			$("#menuloader").html(x);
-			$("#menuloader").show("fade");
-		},
-	});
+	var m=$("#menu").val();
+	if(m!=''){
+		$.ajax({
+			type:'get',
+			dataType:'html',
+			url:'<?=base_url(roleURIUser()."style/menus/getmenu");?>',
+			data:'menu='+m,
+			beforeSend:function(){			
+			},
+			success:function(x){			
+				$("#menuloader").html(x);
+				$("#menuloader").show("fade");
+				$(".globalpos").val(m);
+			},
+		});
+	}else{
+		$(".globalpos").val("");
+		return false;
+	}
 }
 $(document).ready(function(){
-setTimeout(function(){getMenu();},10)
+
+$("#menu").change(function(){
+	getMenu();
+});
+
+setTimeout(function(){$("#menu").trigger("change");},10);
+
 });
 </script>
